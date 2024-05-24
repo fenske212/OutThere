9b2a77ed853ba0d1cedeef5f75039c5c0c8285a9
Brandon Fenske <bafenske@asu.edu>
2024-05-24 03:35:22 -0700

    homepage mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Nav from "@/components/NavMenu";
 import Alert from "@/components/Alert";
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-left bg-orange-50 justify-between ">
       <Header />
-      
+    <div className="flex-grow relative">
+      <Image src="/frontpage1.JPG" alt="Logo" layout="fill"priority />
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-4 text-4xl font-bold text-black">
+        <label className="text-4xl font-bold text-white">OutThere Social Club</label>
+      </div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-4 text-4xl font-bold text-black">
+        <label className="text-4xl font-bold text-white">Addressing the Loneliness Epidemic</label>
+      </div>
+    </div>
+
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,20 @@
 import React from 'react';
 import Image from 'next/image';
 import Nav from '@/components/NavMenu';
+import Link from 'next/link';
 
 export default function Header() {
     return (
     <header className="flex items-left p-4 bg-orange-200 shadow-md">
         <Image src="/dbdm.svg" alt="Logo" width={30} height={15} priority />
-        <h1 className="text-xl font-bold ml-3 items-left self-center">OutThere Social Club</h1>
+        <h1 className="text-xl font-bold ml-3 mr-6 items-left self-center">OutThere Social Club</h1>
+        <Link href="/docs" className="group inline-flex mr-6 h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent/50 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50">
+            <div className="text-sm font-medium leading-none">Members</div></Link>
+        <Link href="/docs" className="group inline-flex mr-6 h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent/50 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50">
+            <div className="text-sm font-medium leading-none">Services</div></Link>    
+        <Link href="/docs" className="group inline-flex mr-6 h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent/50 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50">
+            <div className="text-sm font-medium leading-none">Resources</div></Link>    
+                    
         <div className="flex-grow"></div>
             <Nav />
         <div className="pb-4"></div>
